5495822613853bc53184c07ee9ac95a194888704
Wang GuanHua <30453859+wgh0807@users.noreply.github.com>
2017-12-06 22:53:04 +0800

    Add files via upload
    2017-12-6 first time to uploading my project of cryptology-test1 first edition

---
+++ Form1.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace 密码学大作业2Test2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.openFilleButton = new System.Windows.Forms.Button();
+            this.inputBox = new System.Windows.Forms.TextBox();
+            this.runButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.outputBox = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.importToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exportToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.openFilleButton);
+            this.groupBox1.Controls.Add(this.inputBox);
+            this.groupBox1.Location = new System.Drawing.Point(15, 34);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(317, 131);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "待运算文本输入区";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "MD5",
+            "SHA1"});
+            this.comboBox1.Location = new System.Drawing.Point(81, 104);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(172, 20);
+            this.comboBox1.TabIndex = 4;
+            this.comboBox1.Text = "MD5";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(14, 104);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "算法：";
+            // 
+            // openFilleButton
+            // 
+            this.openFilleButton.Location = new System.Drawing.Point(259, 20);
+            this.openFilleButton.Name = "openFilleButton";
+            this.openFilleButton.Size = new System.Drawing.Size(39, 75);
+            this.openFilleButton.TabIndex = 1;
+            this.openFilleButton.Text = "导入文件";
+            this.openFilleButton.UseVisualStyleBackColor = true;
+            this.openFilleButton.Click += new System.EventHandler(this.openFilleButton_Click);
+            // 
+            // inputBox
+            // 
+            this.inputBox.Location = new System.Drawing.Point(16, 20);
+            this.inputBox.Multiline = true;
+            this.inputBox.Name = "inputBox";
+            this.inputBox.Size = new System.Drawing.Size(237, 75);
+            this.inputBox.TabIndex = 0;
+            // 
+            // runButton
+            // 
+            this.runButton.Location = new System.Drawing.Point(96, 180);
+            this.runButton.Name = "runButton";
+            this.runButton.Size = new System.Drawing.Size(108, 23);
+            this.runButton.TabIndex = 3;
+            this.runButton.Text = "运行";
+            this.runButton.UseVisualStyleBackColor = true;
+            this.runButton.Click += new System.EventHandler(this.runButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(133, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "hash算法计算器";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.outputBox);
+            this.groupBox2.Location = new System.Drawing.Point(15, 209);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(317, 89);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "计算结果显示区";
+            // 
+            // outputBox
+            // 
+            this.outputBox.Location = new System.Drawing.Point(16, 24);
+            this.outputBox.Multiline = true;
+            this.outputBox.Name = "outputBox";
+            this.outputBox.ReadOnly = true;
+            this.outputBox.Size = new System.Drawing.Size(282, 50);
+            this.outputBox.TabIndex = 0;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(364, 25);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.importToolStripMenuItem,
+            this.exportToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(39, 21);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // importToolStripMenuItem
+            // 
+            this.importToolStripMenuItem.Name = "importToolStripMenuItem";
+            this.importToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.importToolStripMenuItem.Text = "import";
+            // 
+            // exportToolStripMenuItem
+            // 
+            this.exportToolStripMenuItem.Name = "exportToolStripMenuItem";
+            this.exportToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exportToolStripMenuItem.Text = "export";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(364, 312);
+            this.Controls.Add(this.runButton);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MinimumSize = new System.Drawing.Size(380, 350);
+            this.Name = "Form1";
+            this.Text = "密码学大作业2-1507030108";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox inputBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox outputBox;
+        private System.Windows.Forms.Button openFilleButton;
+        private System.Windows.Forms.Button runButton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem importToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exportToolStripMenuItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using System.Windows.Forms;
+
+namespace 密码学大作业2Test2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public static string MD5(string encryptString)
+        {
+            byte[] result = Encoding.Default.GetBytes(encryptString);
+            MD5 md5 = new MD5CryptoServiceProvider();
+            byte[] output = md5.ComputeHash(result);
+            string encryptResult = BitConverter.ToString(output).Replace("-", "");
+            return encryptResult;
+        }
+
+        public static string SHA1(string content)
+        {
+            Encoding encode = Encoding.UTF8;
+                SHA1 sha1 = new SHA1CryptoServiceProvider();
+                byte[] bytes_in = encode.GetBytes(content);
+                byte[] bytes_out = sha1.ComputeHash(bytes_in);
+                sha1.Dispose();
+                string result = BitConverter.ToString(bytes_out);
+                result = result.Replace("-", "");
+                return result;
+        }
+
+        private void openFilleButton_Click(object sender, EventArgs e)
+        {
+            inputBox.Text = "";
+            OpenFileDialog file1 = new OpenFileDialog();
+            file1.Filter = "文本文件|*.txt";
+            if (file1.ShowDialog() == DialogResult.OK)
+            {
+                StreamReader sr = File.OpenText(file1.FileName);
+                while (sr.EndOfStream != true)
+                {
+                    inputBox.Text += sr.ReadLine()+"\n";
+                }
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            int l = comboBox1.Text.Length;
+            int i = comboBox1.Text.IndexOf("MD5");
+            int j = comboBox1.Text.IndexOf("SHA1");
+            if(i!=0 && j != 0 || (l!=3 && l!=4))
+            {
+                if (j != -1)
+                {
+                    comboBox1.Text = "SHA1";
+                }
+                else
+                {
+                    comboBox1.Text = "MD5";
+                }
+            }
+        }
+
+        private void runButton_Click(object sender, EventArgs e)
+        {
+            string str = inputBox.Text;
+            if (comboBox1.Text.Equals("SHA1"))
+            {
+                outputBox.Text = SHA1(str);
+            }
+            else
+            {
+                outputBox.Text = MD5(str);
+            }
+        }
+
+       
+        
+    }
+    
+}
